738b602858973ea94c17ca4e00e3c52ae9a982d2
EmyBatista21 <emybatista159@gmail.com>
2024-09-20 15:08:28 -0300

    Manipulando formulários

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,24 +6,54 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      feed:[
-        {id:1, username:'EmyBatista', curtidas:103, comentarios:2},
-        {id:2, username:'Regiane', curtidas:110, comentarios:2},
-        {id:3, username:'Erika', curtidas:1000, comentarios:15},
-        {id:4, username:'Rick', curtidas:1, comentarios:0},
-      ]
+      nome: 'Insira seu nome...',
+      email: 'Insira seu email...',
+      senha: '',
+      genero: 'Insira seu gênero...'
     }
+
+    this.insereNome = this.insereNome.bind(this);
+    this.insereEmail = this.insereEmail.bind(this);
+    this.insereGenero = this.insereGenero.bind(this);
+}
+insereNome(event){
+  this.setState({nome: event.target.value})
 }
 
+insereEmail(event){
+  let valor = event.target.value;
+  this.setState({email: valor})
+}
+
+insereGenero(event){
+  let valor = event.target.value;
+  this.setState({genero: valor})
+}
 
 render(){
     return(
         <div>
-         {this.state.feed.map((item) => {
-            return(
-                <Post key={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
-            );
-         })}
+          <h2>Login</h2>
+          Nome:
+          <input type='name' name='name' value={this.state.nome} onChange={this.insereNome}/> <br/>
+          Email:
+          <input type='email' name='email' value={this.state.email} onChange={this.insereEmail}/> <br/>
+          Senha:
+          <input type='password' name='senha' value={this.state.senha} onChange={(event) => this.setState({senha: event.target.value})}/> <br/> 
+          Gênero:
+          <select name='genero' value={this.state.genero} onChange={this.insereGenero}>
+            <option value='Outros'>Outros</option>
+            <option value='Não binário'>Não binário</option>
+            <option value='Mulher'>Mulher</option>
+            <option value='Homem'>Homem</option>
+          </select>
+
+          <div>
+            <h3>{this.state.nome}</h3>
+            <h3>{this.state.email}</h3>
+            <h3>{this.state.senha}</h3>
+            <h3>{this.state.genero}</h3>
+          </div>
         </div>
     );
 }
