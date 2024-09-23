a9abad4d4b2e885ddf5d744c3cf7079aa78ef071
EmyBatista21 <emybatista159@gmail.com>
2024-09-23 14:45:42 -0300

    useCallBack

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 function App() {
   const [tarefas, setTarefas] = useState([]);
@@ -17,12 +17,17 @@ function App() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
   }, [tarefas]); 
 
-  function addTask(){
+  const addTask = useCallback(() => {
     setTarefas([...tarefas, input]);
     setInput('');
-  }
+  }, [input, tarefas]);
 
-  const totalTarefas = useMemo(() => tarefas.length, [tarefas]); //Só executa esse calculo quando tarefas sofrer uma alteração. O calculo de tarefas não precisará executar dentro do "return" e isso economiza recursos para tarefas custosas.
+  // A quando addTask era função ela está dentro de outra função, então temo duas variaveis 'input' e 'tarefa'. Toda vez que o componente era renderizado a função addTask era recriada, mas só faz sentido ela ser recriada quando tanto 'input' ou 'tarefas' forem atualizada. Então o useCallback é utilizado para memorizar uma função, ou seja, ele garante que uma função será recriada apenas quando alguma de suas dependências mudar. Isso é útil para evitar a criação de novas instâncias de funções desnecessariamente, o que pode melhorar a performance do componente. 
+
+  const totalTarefas = useMemo(() => tarefas.length, [tarefas]); 
+  //Só executa esse calculo quando tarefas sofrer uma alteração. O calculo de tarefas não precisará executar dentro do "return" e isso economiza recursos para tarefas custosas.
+
+
 
   return (
     <div>
