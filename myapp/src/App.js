c45466660386d43f9cd77564aeb0e6c6f3c6b5e8
EmyBatista21 <emybatista159@gmail.com>
2024-09-23 12:54:37 -0300

    Minha pratica useState

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [tarefas, setTarefas] = useState([
-    'Pagar a conta de luz',
-    'Estudar React'
-  ]);
+  const [alunos, setAlunos] = useState([]);
+  const [inputNome, setNome] = useState('');
+  const [inputCurso, setCurso] = useState('');
 
-  const [input, setInput] = useState('');
-
-  function addTask(){
-    setTarefas([...tarefas, input])
-    setInput('');
+  function addInfo(){
+    setAlunos([...alunos, {nome: inputNome, curso: inputCurso}]);
+    setNome('');
+    setCurso('');
   }
 
   return (
     <div>
       <h1>To Do</h1>
       <ul>
-        {tarefas.map(tarefa => (
-          <li key={tarefa}>{tarefa}</li>
+        {alunos.map((aluno, index) => (
+          <li key={index}>
+            {aluno.nome} - {aluno.curso}
+          </li>
         ))}
       </ul>
-      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
-      <button type='button' onClick={addTask}
-      >Adcionar Tarefa</button>
+      <input type='text' placeholder='Nome do Aluno' value={inputNome} onChange={e => setNome(e.target.value)}/> <br/>
+      <input type='text' placeholder='Curso do Aluno' value={inputCurso} onChange={e => setCurso(e.target.value)}/> <br/>
+      <button type='button' onClick={addInfo}
+      >Adcionar Informações</button>
     </div>
   );
 }
