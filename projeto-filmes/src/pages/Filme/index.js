dad18ed5586c8abdf61a978fd6b2b4044c1e1e70
EmyBatista21 <emybatista159@gmail.com>
2024-09-27 18:42:19 -0300

    Criando projeto filmes

---
+++ projeto-filmes/src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme(){
+    return(
+        <div>
+            <h2>Bem vindo a página de detalhes do filme</h2>
+        </div>
+    )
+}
+
+export default Filme;
--- treinando-rotas/src/pages/Home/index.js
+++ treinando-rotas/src/pages/Home/index.js
@@ -6,6 +6,11 @@ function Home() {
         <h1>Welcome to home</h1>
         <Link to="/sobre">Sobre </Link>
         <Link to="/contato">Contato</Link>
+
+        <hr/>
+
+        <Link to="/produto/123">Acessar produto 123</Link> <br/>
+        <Link to="/produto/456">Acessar produto 456</Link>
       </div>
     );
 }
